08dd363fb069f29643577c22a5239d8d668d1076
srfunksensei <sr.funk.sensei@gmail.com>
2020-09-15 12:08:54 +0200

    added flip behaviour

--- CardFlip/CardFlip/ViewController.swift
+++ CardFlip/CardFlip/ViewController.swift
@@ -20,18 +20,42 @@ class ViewController: UIViewController {
         return createView(backgroundColor: .systemYellow)
     }()
     
+    enum ViewType {
+        case front, back
+    }
+    
+    private var currentView: ViewType = .back
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addViewTo(view: frontView, to: containerView)
+        addViewTo(view: backView, to: containerView)
     }
 
     @IBAction func flipView(_ sender: Any) {
+        switch currentView {
+        case .front:
+            UIView.transition(from: frontView, to: backView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: {(success) in
+                self.currentView = .back
+                })
+        case .back:
+            UIView.transition(from: backView, to: frontView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: {(success) in
+            self.currentView = .front
+            })
+        }
     }
     
     fileprivate func createView(backgroundColor: UIColor?) -> UIView {
         let view = UIView()
         view.backgroundColor = backgroundColor
         return view
+    }
+    
+    fileprivate func addViewTo(view: UIView, to: UIView) {
+        view.frame.size = to.frame.size
+        view.frame.origin = CGPoint(x: 0, y: 0)
+        to.addSubview(view)
     }
 }
 
